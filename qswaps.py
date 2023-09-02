cff088f5e182a594036910077aae5c9ccbfee653
Bjørnar Luteberget <luteberget@gmail.com>
2023-09-02 16:24:18 +0200

    second example maybe working

--- qswaps.py
+++ qswaps.py
@@ -1,3 +1,9 @@
+#
+# Cfr.
+#  https://arxiv.org/pdf/2306.08629.pdf
+#  https://arxiv.org/pdf/2208.13679.pdf
+#
+
 from dataclasses import dataclass
 from typing import List, Tuple
 from pysat.formula import IDPool
@@ -75,7 +81,7 @@ def solve(raw_problem: RawProblem):
                 executed_before = vpool.id(f"t{t-1}_g{gate_idx}")
                 solver.add_clause([-executed_before, executed_after])
                 solver.add_clause([-execute, -executed_before])
-                solver.add_clause([-execute, executed_before])
+                solver.add_clause([-execute, executed_after])
                 solver.add_clause([executed_before, -executed_after, execute])
 
             # Find predecessors
@@ -93,158 +99,195 @@ def solve(raw_problem: RawProblem):
                     pred_b = gate_j
 
             # The gate's predecessors have already been executed
-            preds  =[x for x in [pred_a, pred_b] if x is not None]
-            print("gate ", gate_idx, "preds", preds)
+            preds = set(x for x in [pred_a, pred_b] if x is not None)
+            # print("gate ", gate_idx, "preds", preds)
             for pred in preds:
                 if t == 0:
                     # If we have predecessors, and t=0, then we cannot execute.
+                    # print("cannot execute", f"t{t}_g{gate_idx}!")
                     solver.add_clause([-execute])
                 else:
                     solver.add_clause([-execute, vpool.id(f"t{t-1}_g{pred}")])
 
             locations = []
-            for pb1, pb2 in (
-                e for a, b in raw_problem.topology for e in [(a, b), (b, a)]
-            ):
-                execute_at = vpool.id(f"t{t}_g{gate_idx}@({pb1},{pb2})!")
+
+            for edge in raw_problem.topology:
+                # either:
+                #  - gate.line1 maps to edge[0] and gate.line2 maps to edge[1] 
+                #  - or the other way around
+                # it doesn't matter, because we can just flip it around to match.
+                # The names of the bits are anyway tracking the correct information flow,
+                # so we don't need to explicitly model the direction of the edge
+
+                execute_at = vpool.id(f"t{t}_g{gate_idx}@({edge[0]},{edge[1]})!")
                 locations.append(execute_at)
 
                 # At the current time, the logical inputs to the gate must be mapped
                 # to the physical bits.
-
-                for lb,pb in [(gate.line1, pb1), (gate.line2, pb2)]:
-                    cl = [-execute_at, vpool.id(f"t{t}_{lb}@{pb}")]
-                    print("execute_at clause:",cl)
+                for lb in [gate.line1, gate.line2]:
+                    cl = [-execute_at, vpool.id(f"t{t}_{lb}@{edge[0]}"), vpool.id(f"t{t}_{lb}@{edge[1]}")]
                     solver.add_clause(cl)
 
-            # if executing the gate, it has to be executed at one of the possible physical locations
-            print("gate ", gate_idx, "locations", locations)
+            # If executing the gate, it has to be executed at one of the possible physical locations
+            # print("gate ", gate_idx, "locations", locations)
             solver.add_clause([-execute] + locations)
 
         #
         # SWAPS:
-        # Bits can be moved physically, but it takes time.
+        # Bits can move physically, if a swap gate is present.
+        #
 
-        for pb in physical_bits:
-            # possible swaps involving this bit
-            adjacent_edges = [
-                (pb1 if pb == pb2 else pb2, (pb1, pb2))
-                for pb1, pb2 in raw_problem.topology
-                if pb1 == pb or pb2 == pb
-            ]
+        swap_start = t - raw_problem.swap_time
+        if t >= 1:
+            for pb in physical_bits:
+                # possible swaps involving this bit
+                adjacent_edges = {
+                    pb1 if pb == pb2 else pb2: (pb1, pb2)
+                    for pb1, pb2 in raw_problem.topology
+                    if pb1 == pb or pb2 == pb
+                }
 
-            # non-adjacent bits are unreachable
-            if t >= 1:
-                for unreachable_pb in physical_bits:
-                    if unreachable_pb == pb or unreachable_pb in (
-                        a for a, _ in adjacent_edges
-                    ):
-                        continue
+                for other_pb in (b for b in physical_bits if b != pb):
+                    swap_var = None
+                    if swap_start >= 0 and other_pb in adjacent_edges:
+                        other_pb1, other_pb2 = adjacent_edges[other_pb]
+                        swap_var = f"t{swap_start}_sw({other_pb1},{other_pb2})"
 
                     for lb in logical_bits:
+                        # Explanatory frame axiom: if the position changes, we have done the swap
+                        # (there is only one possible swap for a pair of consecutive positions)
+
                         solver.add_clause(
                             [
-                                -vpool.id(f"t{t-1}_{lb}_{unreachable_pb}"),
-                                -vpool.id(f"t{t}_{lb}_{pb}"),
+                                -vpool.id(f"t{t-1}_{lb}@{other_pb}"),
+                                -vpool.id(f"t{t}_{lb}@{pb}"),
                             ]
+                            + ([vpool.id(swap_var)] if swap_var is not None else [])
                         )
 
-            # adjacent bits are reachable if we did a swap
-            for other_pb, (pb1, pb2) in adjacent_edges:
-                if t >= raw_problem.swap_time:
-                    did_swap = vpool.id(f"t{t-raw_problem.swap_time}_sw({pb1},{pb2})")
-                    for lb in logical_bits:
-                        solver.add_clause(
-                            [
-                                -vpool.id(f"t{t-1}_{lb}_{other_pb}"),
-                                -vpool.id(f"t{t}_{lb}_{pb}"),
-                                did_swap,
-                            ]
-                        )
+                        # Action post-condition (effect): if the swap was selected,
+                        # the bits will have to move.
 
-                elif t >= 1:
-                    # have not have time to swap yet
-                    for lb in logical_bits:
-                        solver.add_clause(
-                            [
-                                -vpool.id(f"t{t-1}_{lb}_{other_pb}"),
-                                -vpool.id(f"t{t}_{lb}_{pb}"),
-                            ]
-                        )
+                        if swap_var is not None:
+                            solver.add_clause(
+                                [
+                                    -vpool.id(swap_var),
+                                    -vpool.id(f"t{t-1}_{lb}@{other_pb}"),
+                                    vpool.id(f"t{t}_{lb}@{pb}"),
+                                ]
+                            )
 
-            # If we did swap, the bits are moved after `swap_time`.
-            if t >= raw_problem.swap_time:
-                for other_pb, (pb1, pb2) in adjacent_edges:
-                    for lb in logical_bits:
-                        solver.add_clause(
-                            [
-                                -vpool.id(
-                                    f"t{t-raw_problem.swap_time}_sw({pb1},{pb2})"
-                                ),
-                                -vpool.id(f"t{t-1}_{lb}@{pb}"),
-                                vpool.id(f"t{t}_{lb}@{other_pb}"),
-                            ]
-                        )
-
-        # If we did swap, then we cannot also execute gates or other swaps 
+        # If we did swap, then we cannot also execute gates or other swaps
         # from time `t-swap_time` up to time `t`.
-        if t >= raw_problem.swap_time:
-            for pb1, pb2 in raw_problem.topology:
-                other_edges = [
-                    (a, b)
-                    for a, b in raw_problem.topology
-                    if (a, b) != (pb1, pb2)
-                    and len(set([a, b]).intersection(set([pb1, pb2]))) > 0
+        if swap_start >= 0:
+            for this_edge in raw_problem.topology:
+                adjacent_edges = [
+                    other_edge
+                    for other_edge in raw_problem.topology
+                    if other_edge != this_edge
+                    and len(set(other_edge).intersection(set(this_edge))) > 0
                 ]
 
-                running_swap = vpool.id(f"t{t-raw_problem.swap_time}_sw({pb1},{pb2})")
-                for conflicting_time in range(t-raw_problem.swap_time+1, t+1):
+                this_swap = vpool.id(f"t{swap_start}_sw({this_edge[0]},{this_edge[1]})")
 
-                    # Don't run conflicting swaps
-                    for other_pb1,other_pb2 in other_edges:
-                        solver.add_clause([
-                            -running_swap,
-                            -vpool.id(f"t{conflicting_time}_sw({other_pb1},{other_pb2})")
-                        ])
+                for other_time in range(swap_start, t):
+                    for other_edge in adjacent_edges + [this_edge]:
+                        #
+                        # Gate incompatibility
 
-                    # Don't run any gates on these physical bits
-                    for gate_idx,gate in enumerate(raw_problem.gates):
-                        solver.add_clause([
-                            -running_swap,
-                            -vpool.id(f"t{conflicting_time}_g{gate_idx}@({pb1},{pb2})!")
-                        ])
+                        for gate_idx, _gate in enumerate(raw_problem.gates):
+                            other_gate = vpool.id(
+                                f"t{other_time}_g{gate_idx}@({other_edge[0]},{other_edge[1]})!"
+                            )
+
+                            solver.add_clause([-this_swap, -other_gate])
+
+                        #
+                        # Swap incompatibility
+                        other_swap = vpool.id(
+                            f"t{other_time}_sw({other_edge[0]},{other_edge[1]})"
+                        )
+
+                        if this_swap != other_swap:
+                            solver.add_clause([-this_swap, -other_swap])
 
         n_states += 1
 
-    add_state() # Need at least one state for the assumptions to be non-trivially satisfied
+    add_state()  # Need at least one state for the assumptions to be non-trivially satisfied
 
     while True:
         print(f"solving with {n_states} states...")
-        
-        t = n_states-1
-        all_gates_executed = [f"t{t}_g{gate_idx}" for gate_idx,_ in enumerate(raw_problem.gates)]
 
-        for i,v in vpool.id2obj.items():
-            print("var ", i, ". ", v)
+        t = n_states - 1
+        all_gates_executed = [
+            f"t{t}_g{gate_idx}" for gate_idx, _ in enumerate(raw_problem.gates)
+        ]
+
+        # for i, v in vpool.id2obj.items():
+        #     print("var ", i, ". ", v)
 
         print("clauses", solver.nof_clauses(), "vars", solver.nof_vars())
-        print(solver.accum_stats())
-        print("assumptions", all_gates_executed)
+        # print(solver.accum_stats())
+        # print("assumptions", all_gates_executed)
+
+
         status = solver.solve([vpool.id(v) for v in all_gates_executed])
-        print(f"done status={status}")
+        # print(f"done status={status}")
 
         if status:
+            model = set(solver.get_model())
+
+            # for i, v in vpool.id2obj.items():
+            #     print("var ", i, ". ", v,"=", i in model)
+            #     assert i in model or -i in model
+            # print(model)
+
+
+            for t in range(0,n_states):
+                print(f"@t={t}")
+                for gate_idx,gate in enumerate(raw_problem.gates):
+                    if vpool.id(f"t{t}_g{gate_idx}!") in model:
+                        for e in raw_problem.topology:
+                            if vpool.id(f"t{t}_g{gate_idx}@({e[0]},{e[1]})!") in model:
+                                print(f"  g{gate_idx+1} ({gate.line1},{gate.line2}) at ({e[0]},{e[1]})")
+
+                for e in raw_problem.topology:
+                    if vpool.id(f"t{t}_sw({e[0]},{e[1]})") in model:
+                        print(f"  swap ({e[0]},{e[1]})")
             print("SAT")
             break
         else:
-            if n_states >= 2:
+            if n_states >= 100:
                 raise Exception("UNSAT")
             else:
                 add_state()
 
 
-example1 = RawProblem(
-    gates=[RawGate("l1", "l2", 1), RawGate("l2","l3",1)], topology=[("p1", "p2"), ("p2","p3"), ("p3","p4")], swap_time=3
-)
+if __name__ == "__main__":
 
-solve(example1)
+    
+
+    example1 = RawProblem(
+        gates=[RawGate("l1", "l2", 1), RawGate("l3", "l4", 1), RawGate("l1", "l3", 1), RawGate("l2", "l4", 1)],
+        topology=[("p1", "p2"), ("p2", "p3"), ("p3","p4")],
+        swap_time=3,
+    )
+
+    example2 = "1-(2,4)-3, 2-(3,1)-3, 3-(3,4)-3, 4-(2,1)-3, 5-(2,4)-3, 6-(1,3)-3, 7-(3,2)-3, 8-(4,1)-3"
+    print([parts for g in example2.split(", ") for parts in g.split("-")])
+    
+    example2 = RawProblem(
+        gates=[
+            RawGate(
+                line1="l" + g.split("-")[1][1:].split(",")[0],
+                line2="l" + g.split("-")[1][:-1].split(",")[1],
+                duration=1,
+            )
+            for g in example2.split(", ")
+        ],
+        topology=[("p1", "p2"), ("p2", "p3"), ("p3","p4")],
+        swap_time=3
+    )
+
+    print(example2)
+    solve(example2)
